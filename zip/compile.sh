3e467e9c996b562c466df30c7bea2855eb01c179
Rob Allen <rob@akrabat.com>
2017-06-14 08:09:40 +0100

    Update zip/compile.sh

--- zip/compile.sh
+++ zip/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -rm -it -v "$(pwd):/owexec" openwhisk/swift3action bash -e -c "
+docker run --rm -it -v "$(pwd):/owexec" openwhisk/swift3action bash -e -c "
 if [ -z \"$1\" ] ; then
     echo 'Error: Missing action name'
     exit
@@ -9,13 +9,16 @@ if [ -f \"/owexec/$1.zip\" ] ; then
     rm \"/owexec/$1.zip\"
 fi
 
-# echo 'Setting up build...'
+# echo \"Setting up build...\"
 cp \"/owexec/$1.swift\" /swift3Action/spm-build/main.swift
 cat /swift3Action/epilogue.swift >> /swift3Action/spm-build/main.swift
 echo '_run_main(mainFunction:main)' >> /swift3Action/spm-build/main.swift
-# echo 'Compiling $1...'
+
+# echo \"Compiling $1...\"
 /swift3Action/spm-build/swiftbuildandlink.sh
-# echo 'Creating archive $1.zip...'
+
+
+echo \"Creating archive $1.zip...\"
 cd /swift3Action/spm-build
 zip \"/owexec/$1.zip\" .build/release/Action
 "
